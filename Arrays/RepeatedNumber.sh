2690bc5f8578905eb40b28d1fdbdd79a488f2f4f
kali <sahilkudake7@gmail.com>
2020-06-01 02:35:10 -0400

    Programs on arrays

---
+++ Arrays/RepeatedNumber.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+declare -a repeated
+
+findRepeated() {
+repeate=0
+number=$1
+while [ $number -gt 0 ]
+	do
+		rem=$(( $number % 10 ))
+		repeate=$(( $repeate * 10 + $rem ))
+		number=$(( $number / 10 ))
+	done
+	echo $repeate
+}
+
+read -p "Enter ending number: " END
+for(( i=10;i<=$END;i++ ))
+do
+	repeate=$(findRepeated $i)
+		if [[ $repeate -eq $i ]]
+			then
+			repeated[((k++))]=$i 
+		fi
+done
+	echo "Repeated numbers are: " ${repeated[*]}
---
+++ Arrays/SecondLargestUsingSort.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+first=0
+secound=0
+for((i=1;i<=10;i++))
+   do
+      rand[$i]=$(($RANDOM%400 + 100))
+
+done
+arrSmallest=($(echo ${rand[*]}| tr " " "\n"  | sort -n))
+arrLargest=($(echo ${rand[*]}|   tr " " "\n" | sort -nr))
+echo "Before Sorting: " ${rand[*]}
+echo "After Sorting: " ${arrSmallest[*]}
+echo "Second smallest number is: " ${arrSmallest[1]}
+echo "After Sorting: " ${arrLargest[*]}
+echo "Second Largest Number is: " ${arrLargest[1]}
